3bc197c2d00bada84af54ae9790c14d4085e0c89
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2013-06-14 18:28:37 +0900

    Create dispatcher class.

---
+++ In-app-Billing-Example.Project/In-app-Billing-Example/src/main/java/com/mazgi/android/in_app_billing_example/IABDispatcherDelegate.java
@@ -0,0 +1,7 @@
+package com.mazgi.android.in_app_billing_example;
+
+/**
+ * Created by matsuki_hidenori on 6/14/13.
+ */
+public interface IABDispatcherDelegate {
+}
---
+++ In-app-Billing-Example.Project/In-app-Billing-Example/src/main/java/com/mazgi/android/in_app_billing_example/IAPDispatcher.java
@@ -0,0 +1,22 @@
+package com.mazgi.android.in_app_billing_example;
+
+/**
+ * Created by matsuki_hidenori on 6/14/13.
+ */
+public class IAPDispatcher {
+    public void prepare() {
+
+    }
+    public void loadProducts() {
+
+    }
+    public void purchaseProduct() {
+
+    }
+    public IABDispatcherDelegate getDelegate() {
+        return null;
+    }
+    public void setDelegate(IABDispatcherDelegate delegate) {
+
+    }
+}
